a56b5db7e98934ada852c3cb13fff69f15bfcc13
matiusgg <newpowerexperience2016@gmail.com>
2019-10-16 12:53:30 +0200

    segundo commit, se cambio el diccionario que estaba en la clase por el archivo 'signos.json'

--- horoscopo/Horoscopo.py
+++ horoscopo/Horoscopo.py
@@ -2,7 +2,8 @@
 Clase Horoscopo
 QUE CADA DIA TENGA UN UNICO MENSAJE, QUE SI REINICIAS EL MISMO DIA NO TE CAMBIE LA FRASE SI NO QUE SE MANTENGA
 '''
-
+#* Importamos JSON
+import json
 
 class Horoscopo():
 
@@ -11,56 +12,56 @@ class Horoscopo():
 
     # * diccionario Signo que nos ayudara en los metodos
 
-    signos = {
-        'acuario': {
-            'mes': [1, 2],
-            'dias': [20, 18]
-        },
-        'piscis': {
-            'mes': [2, 3],
-            'dias': [19, 20]
-        },
-        'aries': {
-            'mes': [3, 4],
-            'dias': [21, 20]
-        },
-        'tauro': {
-            'mes': [4, 5],
-            'dias': [21, 20]
-        },
-        'geminis': {
-            'mes': [5, 6],
-            'dias': [21, 20]
-        },
-        'cancer': {
-            'mes': [6, 7],
-            'dias': [21, 20]
-        },
-        'leo': {
-            'mes': [7, 8],
-            'dias': [21, 21]
-        },
-        'virgo': {
-            'mes': [8, 9],
-            'dias': [22, 22]
-        },
-        'libra': {
-            'mes': [9, 10],
-            'dias': [23, 22]
-        },
-        'escorpio': {
-            'mes': [10, 11],
-            'dias': [23, 22]
-        },
-        'sagitario': {
-            'mes': [11, 12],
-            'dias': [23, 20]
-        },
-        'capricornio': {
-            'mes': [12, 1],
-            'dias': [21, 19]
-        }
-    }
+    # signos = {
+    #     'acuario': {
+    #         'mes': [1, 2],
+    #         'dias': [20, 18]
+    #     },
+    #     'piscis': {
+    #         'mes': [2, 3],
+    #         'dias': [19, 20]
+    #     },
+    #     'aries': {
+    #         'mes': [3, 4],
+    #         'dias': [21, 20]
+    #     },
+    #     'tauro': {
+    #         'mes': [4, 5],
+    #         'dias': [21, 20]
+    #     },
+    #     'geminis': {
+    #         'mes': [5, 6],
+    #         'dias': [21, 20]
+    #     },
+    #     'cancer': {
+    #         'mes': [6, 7],
+    #         'dias': [21, 20]
+    #     },
+    #     'leo': {
+    #         'mes': [7, 8],
+    #         'dias': [21, 21]
+    #     },
+    #     'virgo': {
+    #         'mes': [8, 9],
+    #         'dias': [22, 22]
+    #     },
+    #     'libra': {
+    #         'mes': [9, 10],
+    #         'dias': [23, 22]
+    #     },
+    #     'escorpio': {
+    #         'mes': [10, 11],
+    #         'dias': [23, 22]
+    #     },
+    #     'sagitario': {
+    #         'mes': [11, 12],
+    #         'dias': [23, 20]
+    #     },
+    #     'capricornio': {
+    #         'mes': [12, 1],
+    #         'dias': [21, 19]
+    #     }
+    # }
 
     def __init__(self, fecha):
 
@@ -84,20 +85,35 @@ class Horoscopo():
         self.diccFecha['mes'] = splitFecha[1]
         self.diccFecha['dia'] = splitFecha[2]
 
-        for signoLlave, valorFecha in self.signos.items():
+        mesInput = int(self.diccFecha['mes'])
+        diaInput = int(self.diccFecha['dia'])
 
-            for llave, valor in valorFecha.items():
+        #* Abrimos el archivo signo.json donde tenemos el diccionario de signos para poder retornarnos el signo
 
-                for i in valor:
+        with open('static/json/signos.json') as contenido:
 
-                    mesInput = int(self.diccFecha['mes'])
-                    diaInput = int(self.diccFecha['dia'])
+            diccionarioSignos = json.load(contenido)
 
-                    if llave == 'mes':
+            for i in diccionarioSignos:
 
-                        if ((valorFecha['mes'][0] == mesInput) and (diaInput >= valorFecha['dias'][0])) or ((valorFecha['mes'][1] == mesInput) and (diaInput <= valorFecha['dias'][1])):
+                if ((i.get('mes')[0] == mesInput) and (diaInput >= i.get('dias')[0])) or ((i.get('mes')[1] == mesInput) and (diaInput <= i.get('dias')[1])):
 
-                            return signoLlave
+                    return i.get('signo')
+
+        # for signoLlave, valorFecha in self.signos.items():
+
+        #     for llave, valor in valorFecha.items():
+
+        #         for i in valor:
+
+        #             mesInput = int(self.diccFecha['mes'])
+        #             diaInput = int(self.diccFecha['dia'])
+
+        #             if llave == 'mes':
+
+        #                 if ((valorFecha['mes'][0] == mesInput) and (diaInput >= valorFecha['dias'][0])) or ((valorFecha['mes'][1] == mesInput) and (diaInput <= valorFecha['dias'][1])):
+
+        #                     return signoLlave
 
 
 # objHoroscopo = Horoscopo('2019-5-21')
