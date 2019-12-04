138b6d5fba056b051faa5932b29d128d3becbb8f
matiusgg <newpowerexperience2016@gmail.com>
2019-12-04 16:03:36 +0100

    puede hacer el primer lanzamiento, pero el resto aún no

--- dados/Dados.py
+++ dados/Dados.py
@@ -6,4 +6,96 @@ class Dados():
 
     def __init__(self):
 
-        self.dado = []
+        #*Lista donde ira la puntuación del usuario
+        self.listaPuntuacion = []
+
+        #*Lista donde ira el números de caras según el tipo que escogio el usuario
+        self.NumCaras = []
+
+        #*Lista con los tipos de dados que si tiene una cantidad
+        self.tiposConCantidad = []
+
+
+        #* Random no iniciado sobre la cual cara sacara
+        self.randomCara = 0
+
+        #*Cantidad total de veces
+        self.cantidadTotal = 0
+
+        #* números de intentos
+        self.intentos = 0
+
+    def activarJuego(self, listaPuntuacion, listaTipos, activar):
+
+        cantidadCaras = 0
+
+        if activar == 'activar':
+
+            print(listaPuntuacion)
+
+            for i in listaPuntuacion:
+
+                if i[1] == '0':
+
+                    print(f'No se coloco una cantidad en este tipo: {i[0]}')
+            
+                else:
+
+                    self.tiposConCantidad.append(i)
+
+            for i in self.tiposConCantidad:
+
+                print('****???*****')
+                print(i)
+
+                self.cantidadTotal += int(i[1])
+
+            for i in self.tiposConCantidad:
+
+                if i[0] == 'cuatro':
+
+                    cantidadCaras = 4
+                    self.randomCara = random.randint(0, cantidadCaras)
+
+                    print(self.randomCara)
+
+                    self.intentos += 1
+
+                    return i[0], self.intentos, self.randomCara, self.cantidadTotal
+
+            
+                if i[0] == 'seis':
+
+                    cantidadCaras = 6
+                    self.randomCara = random.randint(0, cantidadCaras)
+
+                    print(self.randomCara)
+
+                    self.intentos += 1
+
+                    return i[0], self.intentos, self.randomCara, self.cantidadTotal
+
+                if i[0] == 'ocho':
+
+                    cantidadCaras = 8
+                    self.randomCara = random.randint(0, cantidadCaras)
+
+                    print(self.randomCara)
+
+                    self.intentos += 1
+
+                    return i[0], self.intentos, self.randomCara, self.cantidadTotal
+
+                if i[0] == 'diez':
+
+                    cantidadCaras = 10
+                    self.randomCara = random.randint(0, cantidadCaras)
+
+                    print(self.randomCara)
+
+                    self.intentos += 1
+
+                    return i[0], self.intentos, self.randomCara, self.cantidadTotal
+
+
+
--- main.py
+++ main.py
@@ -2,69 +2,77 @@ from flask import Flask, url_for, session, request, redirect, render_template
 from dados.Dados import Dados
 import random
 import csv
-#*Importar MONGO DB
+# *Importar MONGO DB
 from pymongo import MongoClient
 
+# *DATOS BASE DE DATOS EN LA NUBE
+'''
+usuario: mongodb
+contraseña:mongodb
+'''
 
-#* Inicializar nuestra clase de conexion a la BD
+# * Inicializar nuestra clase de conexion a la BD
 
 
-#* Intanciar Flask
+# * Intanciar Flask
 app = Flask(__name__)
 
-#* Crear un llave/clave secreta para SESSION
+# * Crear un llave/clave secreta para SESSION
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
-#**************************************
-#* ULR Conexion
+# **************************************
+# * ULR Conexion
 MONGO_URL_ATLAS = 'mongodb+srv://mongodb:mongodb@cluster0-l6v7e.mongodb.net/test?retryWrites=true&w=majority'
 
-#* Establecer conexion
+# * Establecer conexion
 client = MongoClient(MONGO_URL_ATLAS, ssl_cert_reqs=False)
 
-#* Creacion base de datos
+# * Creacion base de datos
 db = client['ahorcadito']
 
-#* Creacion coleccion
+# * Creacion coleccion
 collectionUsuarios = db['usuarios']
 
+# * Creacion coleccion
+collectionPuntuacion = db['puntuacion']
 
-#******************************************
+
+# ******************************************
 @app.route('/')
 def redireccionar():
 
     return redirect(url_for('home'))
 
-#******************************************
+# ******************************************
 @app.route('/home', methods=['GET', 'POST'])
 def home():
 
     if request.method == 'POST':
 
         session.clear()
-    
 
-    if 'usuario' in session:
+    # if 'usuario' in session:
 
-        return redirect(url_for('juegoDados'))
+    #     return redirect(url_for('tipoDados'))
 
     return render_template('home.html')
+
 
 @app.route('/usuario')
 def usuario():
 
     return render_template('usuario.html')
 
-#******************************************
+# ******************************************
 @app.route('/usuario', methods=['POST'])
 def usuariodatos():
 
-    #* Lista que almacena al usuario
+    # * Lista que almacena al usuario
     listaUsuarioCorrecto = []
 
     usuario = request.form['usuario']
 
-    leer_usuario = collectionUsuarios.find( {'usuario':f'{usuario}'} )
+    leer_usuario = collectionUsuarios.find({'usuario': f'{usuario}'})
 
     # print(list(leer_usuario))
 
@@ -77,15 +85,14 @@ def usuariodatos():
 
     if listaUsuarioCorrecto != []:
 
-
         if listaUsuarioCorrecto[0] == usuario:
-            #* iniciar sesion 
-            #* Limpiamos la session cada vez que haga una nueva session.
+            # * iniciar sesion
+            # * Limpiamos la session cada vez que haga una nueva session.
             session.clear()
             session['usuario'] = usuario
             print('session creada')
 
-            # return redirect(url_for('intentos'))
+            return redirect(url_for('tipoDados'))
 
         else:
 
@@ -97,15 +104,14 @@ def usuariodatos():
     return render_template('usuario.html')
 
 
-
-#******************************************
+# ******************************************
 @app.route('/registro')
 def registro():
 
     return render_template('registro.html')
 
 
-#******************************************
+# ******************************************
 @app.route('/registro', methods=['GET', 'POST'])
 def registroDatos():
 
@@ -113,32 +119,88 @@ def registroDatos():
 
         usuario = request.form['usuario']
 
-        leer_usuario = collectionUsuarios.find( {'usuario':f'{usuario}'} )
+        leer_usuario = collectionUsuarios.find({'usuario': f'{usuario}'})
 
         # print(list(leer_usuario))
 
-        #* Si en la BD no esta vacio, entonces el email que se ingreso en el input existe en la BD
+        # * Si en la BD no esta vacio, entonces el email que se ingreso en el input existe en la BD
         if list(leer_usuario) != []:
 
             return render_template('registro.html', usuario_existe=True)
 
         collectionUsuarios.insert_one({"usuario": usuario})
 
-
         return redirect(url_for('usuario'))
-
-        
 
     return render_template('registro.html')
 
-# #******************************************
 
-#******************************************
+# ******************************************
+listaTipos = ['cuatro', 'seis', 'ocho', 'diez']
+# ******************************************
+@app.route('/tipoDados')
+def tipoDados():
+
+    return render_template('tipoDados.html', listaTipos=listaTipos)
+
+
+# *****************************************
+
+# ******************************************
+@app.route('/juegoDados')
+def juegoDados():
+
+    return render_template('juegoDados.html')
+
+
+# ******************************************
+
+
+@app.route('/juegoDados', methods=['POST'])
+def juegoDadosDatos():
+
+    activar = request.form['activar']
+
+    # * Objeto juego Dados
+    objDados = Dados()
+
+    listaPuntuacion = []
+
+    for i in listaTipos:
+
+        tipo = request.form[f'{i}']
+        cantidad = request.form[f'cantidad{i}']
+
+        listaPuntuacion.extend([[tipo, cantidad]])
+
+    (tipoDado, intento, randomCara, cantidadTotal) = objDados.activarJuego(
+        listaPuntuacion, listaTipos, activar)
+
+    # * session usuario
+
+    usuario = session['usuario']
+
+    insertarPuntuacion = collectionPuntuacion.insert_one(
+        {'usuario': f'{usuario}', 'tipoDado': f'{tipoDado}', 'intento': f'{intento}', 'randomCara': f'randomCara', 'cantidadTotal': f'{cantidadTotal}'})
+
+    # if intento < cantidadTotal:
+
+    #     return render_template('juegoDatos.html', tipoDado=tipoDado, intento=intento, random=randomCara, cantidadTotal=cantidadTotal)
+
+    # else:
+
+    #     return render_template('juegoDados.html', gameOver=True)
+
+    return render_template('juegoDados.html', tipoDado=tipoDado, intento=intento, random=str(randomCara), cantidadTotal=cantidadTotal)
+
+
+# ******************************************
 @app.errorhandler(404)
 def page_no_found(error):
     return '<h1> Pagina no encontrada, siga buscando</h1>'
 
-#******************************************
+
+# ******************************************
 if __name__ == "__main__":
 
     app.run('0.0.0.0', '5000', debug=True)
