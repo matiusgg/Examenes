21f0dd018862b9ceb7542624dca32fae763a9cf8
matiusgg <newpowerexperience2016@gmail.com>
2020-01-16 16:49:30 +0100

    se han agregado login para la ruleta

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # * IMPORTACIONES del FLASK y del PAQUETE donde se alberga la CLASE Horoscopo()
 from flask import Flask, request, make_response, redirect, render_template, session, url_for
+
 from ruleta.Ruleta import Ruleta
+
+import random
+import csv
+# *Importar MONGO DB
+from pymongo import MongoClient
 
 # *****************************************
 # * variable APP
@@ -11,6 +17,26 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 # *****************************************
+
+# **************************************
+# * ULR Conexion
+MONGO_URL_ATLAS = 'mongodb+srv://mongodb:mongodb@cluster0-l6v7e.mongodb.net/test?retryWrites=true&w=majority'
+
+# * Establecer conexion
+client = MongoClient(MONGO_URL_ATLAS, ssl_cert_reqs=False)
+
+# * Creacion base de datos
+db = client['examen_m3_final']
+
+# * Creacion coleccion
+collectionUsuarios = db['usuarios']
+
+#*****************************************
+
+# Objeto
+# ruletaObj = Ruleta()
+
+#*******************************************
 
 # *Ruta que direcciona a '/home'
 @app.route('/')
@@ -26,6 +52,100 @@ def redireccionar():
 def home():
 
     return render_template('home.html')
+
+# ******************************************
+@app.route('/home', methods=['GET', 'POST'])
+def homeDatos():
+
+    if request.method == 'POST':
+
+        session.clear()
+
+    if 'usuario' in session:
+
+        return redirect(url_for('tipoDados'))
+
+    return render_template('home.html')
+
+
+#********************************************
+
+
+@app.route('/usuario')
+def usuario():
+
+    return render_template('usuario.html')
+
+# ******************************************
+@app.route('/usuario', methods=['POST'])
+def usuariodatos():
+
+    # * Lista que almacena al usuario
+    listaUsuarioCorrecto = []
+
+    usuario = request.form['usuario']
+
+    leer_usuario = collectionUsuarios.find({'usuario': f'{usuario}'})
+
+    # print(list(leer_usuario))
+
+    for i in leer_usuario:
+
+        print(i['usuario'])
+        listaUsuarioCorrecto.append(i['usuario'])
+
+    # print(listaUsuarioCorrecto[0])
+
+    if listaUsuarioCorrecto != []:
+
+        if listaUsuarioCorrecto[0] == usuario:
+            # * iniciar sesion
+            # * Limpiamos la session cada vez que haga una nueva session.
+            session.clear()
+            session['usuario'] = usuario
+            print('session creada')
+
+            return redirect(url_for('reglas'))
+
+        else:
+
+            return render_template('usuario.html', no_usuario=True)
+    else:
+
+        return render_template('usuario.html', no_usuario=True)
+
+    return render_template('usuario.html')
+
+
+# ******************************************
+@app.route('/registro')
+def registro():
+
+    return render_template('registro.html')
+
+
+# ******************************************
+@app.route('/registro', methods=['GET', 'POST'])
+def registroDatos():
+
+    if request.method == 'POST':
+
+        usuario = request.form['usuario']
+
+        leer_usuario = collectionUsuarios.find({'usuario': f'{usuario}'})
+
+        # print(list(leer_usuario))
+
+        # * Si en la BD no esta vacio, entonces el email que se ingreso en el input existe en la BD
+        if list(leer_usuario) != []:
+
+            return render_template('registro.html', usuario_existe=True)
+
+        collectionUsuarios.insert_one({"usuario": usuario})
+
+        return redirect(url_for('usuario'))
+
+    return render_template('registro.html')
 
 # *****************************************
 
@@ -72,35 +192,21 @@ def intentos():
 
 # *****************************************
 
-# # *Ruta RULETA
-# @app.route('/ruleta', methods=['GET'])
-# def ruleta():
+# *Ruta RULETA
+@app.route('/ruleta')
+def ruleta():
 
-#     return render_template('ruleta.html')
+    return render_template('ruleta.html')
 
 # *****************************************
 
 # *Ruta LLEGADA POST RULETA
 @app.route('/ruleta', methods=['POST'])
-def ruleta():
-
-    #*Valor input para activar ruleta
-    inputRuleta = request.form['ruleta']
-
-    # # * Modo intento escogido
-    # intento = request.form['intento']
-
-    #* Objeto
-    ruletaObj = Ruleta()
-
-    #* metodo Ruleta()
-    listaRandom = ruletaObj.ruleta(inputRuleta)
-
-    # #*resultadoTotal
-    # listaResultado = ruletaObj.resultado()
+def ruletaDatos():
 
 
-    return render_template('ruleta.html', opcion=listaRandom[0], splitOpcion=listaRandom[1], resultado=listaRandom[2])
+
+    return render_template('ruleta.html')
 
 # *****************************************
 
--- ruleta/Ruleta.py
+++ ruleta/Ruleta.py
@@ -10,408 +10,52 @@ import csv
 
 class Ruleta():
 
-    # * Diccionario Opciones
-
-    opcionesDicc = {
-        'suma': {
-            'dinero 1000': 1000,
-            'dinero 500': 500,
-            'dinero 200': 200,
-            'dinero 100': 100,
-            'dinero 5000': 5000,
-        },
-        'x2': 2,
-
-        'dinero Mitad': 2,
-        'pierdes': 1,
-        'carcel': 1000,
-        'cosas': ['coche', 'ordenador', 'nevera', 'moto', 'bicicleta'],
-        'saltar': 'saltar',
-        'bote': ['bote', 'bote x2']
-    }
-
     def __init__(self):
 
-        # * Opciones Lista
+        # * Diccionario Opciones
+
+        opcionesDicc = {
+            'suma': {
+                'dinero 1000': 1000,
+                'dinero 500': 500,
+                'dinero 200': 200,
+                'dinero 100': 100,
+                'dinero 5000': 5000,
+            },
+            'x2': 2,
+
+            'dinero Mitad': 2,
+            'pierdes': 1,
+            'carcel': 1000,
+            'cosas': ['coche', 'ordenador', 'nevera', 'moto', 'bicicleta'],
+            'saltar': 'saltar',
+            'bote': ['bote', 'bote x2']
+        }
+
+        # self.inputRuleta = inputRuleta
 
         self.OPCIONES = ['dinero 1000', "pierdes", 'dinero Mitad',
                          "x2", 'dinero 500', 'dinero 200', 'carcel', 'dinero 5000', 'dinero 100', 'coche', 'ordenador', 'nevera', 'moto', 'bicicleta', 'saltar', 'bote', 'bote x2']
 
-        self.aleatorio = random.choice(self.OPCIONES)
+    #*************************************
 
-        self.score = 1
+    def Programa(self):
 
-        self.ListaScore = []
+        if self.inputRuleta == 'activar':
 
-        # self.intentos = intentos
+            randomOpcion = random.choice(self.OPCIONES)
 
-        #* Calculos
-        self.calculos = []
+            for llaveDinero, valorDinero in self.opcionesDicc.items():
 
-        #*ListaDinero
-        self.listaDinero = ['dinero 1000',
-            'dinero 500',
-            'dinero 200',
-            'dinero 100',
-            'dinero 5000']
+                if randomOpcion == llaveDinero:
 
-        #*REsultado
-        self.resultado = []
+                    escribir = open('dinero.csv', 'a', newline='')
 
+                    salida = csv.writer(escribir)
 
-# ****************************************************************
+                    salida.writerow(['opcion', 'score'])
 
-        # * metodo guardarOpcion()
-    def guardarOpcion(self, opcion, score):
+                    salida.writerow([(opcion), (score)])
 
-        escribir = open('score.csv', 'a', newline='')
-
-        salida = csv.writer(escribir)
-
-        salida.writerow(['opcion', 'score'])
-
-        salida.writerow([(opcion), (score)])
-
-        # del salida
-        escribir.close()
-
-# *****************************************************************
-
-    # * metodo mostrarScore()
-    def mostrarScore(self):
-
-        base = []
-        afectanBase = []
-        contador = 0
-        
-
-        with open('score.csv', 'r') as File:
-
-            reader = csv.reader(File)
-
-            for row in reader:
-
-                print('**************************')
-
-                print(row)
-
-                if row[0] != 'opcion' and row[1] != 'score':
-
-
-                    self.calculos.extend([[row[0], row[1]]])
-
-
-        print('*?*?'*50)
-
-        print(self.calculos)
-
-        print('*?*?'*50)
-
-        for minilista in self.calculos:
-
-            if ('dinero 100' in minilista) or ('dinero 200' in minilista) or ('dinero 500' in minilista) or ('dinero 1000' in minilista) or ('dinero 5000' in minilista):
-
-                if minilista[0] == 'dinero 100':
-
-                    base.append(minilista[1])
-
-                if minilista[0] == 'dinero 200':
-
-                    base.append(minilista[1])
-                
-                if minilista[0] == 'dinero 500':
-
-                    base.append(minilista[1])
-                
-                if minilista[0] == 'dinero 1000':
-
-                    base.append(minilista[1])
-                
-                if minilista[0] == 'dinero 5000':
-
-                    base.append(minilista[1])
-
-
-            else:
-
-                print('no se encuentra ninguna opcion de dinero')
-
-            if minilista[0] == 'x2':
-
-                print(minilista)
-
-                afectanBase.append(f'x{minilista[1]}')
-
-            if minilista[0] == 'pierdes':
-
-                print(minilista)
-
-                afectanBase.append(minilista[1])
-
-            if minilista[0] == 'dinero Mitad':
-
-                print(minilista)
-
-                afectanBase.append(f'/{minilista[1]}')
-
-            if minilista[0] == 'carcel':
-
-                print(minilista)
-
-                afectanBase.append(f'-{minilista[1]}')
-
-        print('/opem/'*50)
-
-        for i in base:
-
-            print(i)
-
-            contador += int(i)
-        
-        print(f'base total sin operaciones: {contador}')
-
-        print('/opem2/'*50)
-
-        print(afectanBase)
-
-        #********************************************************************
-
-        listmult = []
-
-        for operacionmult in afectanBase:
-
-            if operacionmult == 'x2':
-
-                listmult.append(operacionmult)
-
-        print(listmult)
-
-        for m in listmult:
-
-            splitMult = m.split('x')
-
-            contador *= int(splitMult[1])
-
-        self.resultado.append(contador)
-
-        print(f'base total multiplicada : {contador}')
-
-        #********************************************************
-
-        listdiv = []
-
-        for operaciondiv in afectanBase:
-
-            if operaciondiv == '/2':
-
-                listdiv.append(operaciondiv)
-
-        print(listdiv)
-
-        for d in listdiv:
-
-            splitdiv = d.split('/')
-
-            contador /= int(splitdiv[1])
-
-        self.resultado.append(contador)
-
-        print(f'base total dividir : {contador}')
-
-        #********************************************************
-
-        listCarcel = []
-
-        for operacionCarcel in afectanBase:
-
-            if operacionCarcel == '-1000':
-
-                listCarcel.append(operacionCarcel)
-
-
-        print(listCarcel)
-
-        for d in listCarcel:
-
-            splitCarcel = d.split('-')
-
-            contador -= int(splitCarcel[1])
-
-        self.resultado.append(contador)
-
-        print(f'base total carcel : {contador}')
-
-        #********************************************************
-
-        if '1' in afectanBase:
-
-            contador = 1
-
-        self.resultado.append(contador)
-
-        print(f'base total pierdes : {contador}')
-
-        print(self.resultado)
-
-
-
-    #*metodo ruleta
-
-    def ruleta(self, inputRuleta):
-
-        if inputRuleta == 'activar':
-
-            # self.intentos += 1
-
-            # print(self.intentos)
-
-            for llave, valor in self.opcionesDicc.items():
-
-                if llave == 'suma':
-
-                    for llaveSuma, valorSuma in valor.items():
-
-                        if self.aleatorio == llaveSuma:
-
-                            suma = self.score + valorSuma
-
-                            separacion = llaveSuma.split(' ')
-
-                            self.ListaScore.append(separacion[0])
-
-                            self.ListaScore.append(separacion[1])
-                            
-                            self.ListaScore.append(valorSuma)
-
-                            self.guardarOpcion(llaveSuma, valorSuma)
-                            self.mostrarScore()
-                            print(self.resultado)
-
-                            return self.ListaScore
-
-                            
-
-                if self.aleatorio == llave and llave == 'carcel':
-
-                    resta = self.score - valor
-
-                    self.ListaScore.append(llave)
-                    self.ListaScore.append(' ')
-                    self.ListaScore.append(self.resultado[2])
-
-                    print(self.ListaScore)
-                    self.guardarOpcion(llave, valor)
-                    self.mostrarScore()
-                    print(self.resultado)
-
-                    return self.ListaScore
-
-                    
-
-                if self.aleatorio == llave and llave == 'x2':
-
-                    mult = self.score * valor
-
-                    self.ListaScore.append(llave)
-                    self.ListaScore.append(' ')
-                    self.ListaScore.append(self.resultado[0])
-
-                    print(self.ListaScore)
-                    self.guardarOpcion(llave, valor)
-                    self.mostrarScore()
-                    print(self.resultado)
-
-                    return self.ListaScore
-
-                    
-
-                if self.aleatorio == llave and llave == 'dinero Mitad':
-
-                    mitdinero = self.score / valor
-
-                    separacion = llave.split(' ')
-
-                    self.ListaScore.append(separacion[0])
-                    self.ListaScore.append(separacion[1])
-                    self.ListaScore.append(self.resultado[1])
-
-                    print(self.ListaScore)
-                    self.guardarOpcion(llave, valor)
-                    self.mostrarScore()
-                    print(self.resultado)
-
-                    return self.ListaScore
-
-                    
-
-                if self.aleatorio == llave and llave == 'pierdes':
-
-                    pierde = self.score
-
-                    self.ListaScore.append(llave)
-                    self.ListaScore.append(' ')
-                    self.ListaScore.append(pierde)
-
-                    print(self.ListaScore)
-                    self.guardarOpcion(llave, pierde)
-                    self.mostrarScore()
-                    print(self.resultado)
-
-                    return self.ListaScore
-
-                    
-
-                if llave == 'cosas':
-
-                    for cosa in valor:
-
-                        if self.aleatorio == cosa:
-
-                            self.ListaScore.append(cosa)
-                            self.ListaScore.append(' ')
-                            self.ListaScore.append(' ')
-
-                            print(self.ListaScore)
-                            self.guardarOpcion(llave, cosa)
-                            self.mostrarScore()
-                            print(self.resultado)
-
-                            return self.ListaScore
-
-                            
-
-                if self.aleatorio == llave and llave == 'saltar':
-
-                    self.ListaScore.append(llave)
-                    self.ListaScore.append(' ')
-                    self.ListaScore.append(valor)
-
-                    print(self.ListaScore)
-                    self.guardarOpcion(llave, valor)
-                    self.mostrarScore()
-                    print(self.resultado)
-
-                    return self.ListaScore
-
-                    
-
-                if llave == 'bote':
-
-                    for botes in valor:
-
-                        if self.aleatorio == botes:
-
-                            self.ListaScore.append(llave)
-                            self.ListaScore.append(' ')
-                            self.ListaScore.append(botes)
-
-                            print(self.ListaScore)
-                            self.guardarOpcion(llave, botes)
-                            self.mostrarScore()
-                            print(self.resultado)
-
-                            return self.ListaScore
-
-                            
-
-                
+                    # del salida
+                    escribir.close()
