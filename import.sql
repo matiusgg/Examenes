d7e34c936e88773663fffda51ea600b054d8cbb1
matiusgg <newpowerexperience2016@gmail.com>
2020-03-10 18:10:20 +0100

    Se han agregado varias cosas. 10/03/2020

---
+++ import.sql
@@ -0,0 +1,3 @@
+/* Populate table */
+INSERT INTO clientes (id, nombre, email, create_at) VALUES(1, 'Andres', 'Guzman', 'profesor@blosadeidea.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, email, create_at) VALUES(1, 'John', 'Doe', 'jon.doe@gmail.com', '2016-08-28');
