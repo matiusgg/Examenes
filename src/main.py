4d02a1a95792870ce3499c6b9d9cb38adf0df169
matiusgg <newpowerexperience2016@gmail.com>
2020-03-30 18:58:49 +0200

    Inicializando el examen

---
+++ src/main.py
@@ -0,0 +1,352 @@
+from flask import Flask, url_for, session, request, redirect, render_template
+#* Importación Paquete con la clase
+from paquete.Sorteos import Sorteos
+#*Random
+import random
+#* CSV
+import csv
+# *Importar MONGO DB
+from pymongo import MongoClient
+#* Libreria Documentación
+import doctest
+#* necesitamos esta libreria que nos ayudará con el if main.
+import os
+
+
+# *DATOS BASE DE DATOS EN LA NUBE
+'''
+usuario: mongodb
+contraseña:mongodb
+'''
+
+# * Intanciar Flask
+app = Flask(__name__)
+
+# * Crear un llave/clave secreta para SESSION
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
+
+# **************************************
+# * ULR Conexion
+MONGO_URL_ATLAS = 'mongodb+srv://mongodb:mongodb@cluster0-l6v7e.mongodb.net/test?retryWrites=true&w=majority'
+
+# * Establecer conexion
+client = MongoClient(MONGO_URL_ATLAS, ssl_cert_reqs=False)
+
+# * Creacion base de datos MongoDB
+db = client['examen_m4_c3']
+
+# * Creacion coleccion EMAIL, donde estarán los registros de email de los usuarios para el sorteo
+collectionEmail = db['collectionEmail']
+
+# * Creacion coleccion
+collectionSorteos = db['collectionSorteos']
+
+
+
+#*******************************************
+
+#* Objeto
+# objetoClase = nombreClase(collection, session['usuario])
+
+
+#*******************************************
+
+# * Coleccion con la información de la Inmobiliaría
+
+# ******************************************
+#* RUTE REDIRECCIONAR
+@app.route('/')
+def redireccionar():
+
+    """
+    RUTA REDIRECCIONAR A LA RUTA HOME
+    """
+
+    return redirect(url_for('home'))
+
+# ******************************************
+#* RUTE HOMRE
+@app.route('/home')
+def home():
+
+    """
+    RUTA HOME
+    """
+
+    if 'email' in session:
+
+        print(f'todavía existe la session: ' + session['email'])
+
+        return render_template('home.html', email_existe=True, email=session['email'])
+
+    else:
+
+        return render_template('home.html', email_existe=False)
+
+    return render_template('home.html')
+
+# ******************************************
+#* Si tienes SESSION, que cuando vuelva a HOME, toda la sección que tengas se borre, en este
+#* caso es con POST, es decir, que según la página en la que estes, el boton para volver a home.html
+# #* debe tener un FORM de POST.
+#* Por sí lo necesitas
+@app.route('/home', methods=['GET', 'POST'])
+def homeDatos():
+
+    """
+    LLEGADA DE DATOS A LA RUTA HOME, ESTO ES PARA PODER LIMPIAR LA SESSION CUANDO VUELVAN A HOME A TRAVEZ DE LA APLICACIÓN.
+    """
+
+    if request.method == 'POST':
+
+        session.clear()
+
+    return render_template('home.html')
+
+#************************************************
+
+#* Login por si lo necesitas
+#* Solo le falta la contraseña, simplemente sería agregarsela
+@app.route('/usuario')
+def usuario():
+
+    """
+    RUTA USUARIO, PARA INGRESAR EL LOGIN. USUARIO Y CONTRAÑESA, O SOLO USUARIO DEPENDIENDO DEL CASO
+    """
+
+    return render_template('usuario.html')
+
+
+
+#************************************************
+
+#* Login por si lo necesitas
+@app.route('/usuario', methods=['GET'])
+def usuarioGet():
+
+    """
+    RUTA USUARIO, PARA INGRESAR EL LOGIN. USUARIO Y CONTRAÑESA, O SOLO USUARIO DEPENDIENDO DEL CASO
+    """
+
+    if request.method == 'GET':
+
+        session.clear()
+
+    return render_template('usuario.html')
+
+# ******************************************
+@app.route('/usuario', methods=['POST'])
+def usuariodatos():
+
+    """
+    LLEGADA DATOS DE USUARIO, CON LOS DATOS DEL LOGIN, PARA PODER INGRESAR A LA APLICACIÓN EN SÍ.
+    """
+
+    # * Lista que almacena al usuario
+    listaemailCorrecto = []
+
+    email = request.form['email']
+
+    leer_email = collectionEmail.find({'email': f'{email}'})
+
+    # print(list(leer_usuario))
+
+    for i in leer_email:
+
+        print(i['email'])
+        listaemailCorrecto.extend([i['email'], str(i['_id'])])
+
+    # print(listaUsuarioCorrecto[0])
+
+    if listaemailCorrecto != []:
+
+        if listaemailCorrecto[0] == email:
+            # * iniciar sesion
+            # * Limpiamos la session cada vez que haga una nueva session.
+            session.clear()
+            session['email'] = email
+            session['email_id'] = listaemailCorrecto[1]
+            print('session creada')
+
+            agregarEmail_id = collectionEmail.update_one({'email': f'{email}'}, {'$set': {'email_id': listaemailCorrecto[1]}})
+
+            return redirect(url_for('home'))
+
+        else:
+
+            return render_template('usuario.html', no_email=True)
+    else:
+
+        return render_template('usuario.html', no_email=True)
+
+    return render_template('usuario.html')
+
+
+# ******************************************
+@app.route('/registro')
+def registro():
+
+    """
+    RUTA REGISTRO, PARA REGISTRARSE EN LA APLICACIÓN
+    """
+
+    return render_template('registro.html')
+
+
+# ******************************************
+@app.route('/registro', methods=['GET', 'POST'])
+def registroDatos():
+
+    """
+    LLEGADA DE DATOS A LA RUTA REGISTRO. PARA REGISTRAR AL USUARIO EN LA APLICACIÓN
+    """
+
+    if request.method == 'POST':
+
+        email = request.form['email']
+
+        leer_email = collectionEmail.find({'email': f'{email}'})
+
+        # print(list(leer_usuario))
+
+        # * Si en la BD no esta vacio, entonces el email que se ingreso en el input existe en la BD
+        if list(leer_email) != []:
+
+            return render_template('registro.html', correo_existe=True)
+
+        collectionEmail.insert_one({"email": email})
+
+        agregarEmail_id = collectionEmail.update_one({'email': f'{email}'}, {'$set': {'email_id': session['email_id']}})
+
+        return redirect(url_for('home'))
+
+    return render_template('registro.html')
+
+
+# ******************************************
+
+
+@app.route('/listaSorteos')
+def listaSorteos():
+
+    """
+    RUTA NOMBRERUTA, TE PERMITE
+    """
+
+    return render_template('listaSorteos.html')
+
+
+# ******************************************
+
+
+@app.route('/listaSorteos', methods=['POST'])
+def listaSorteosDatos():
+
+    """
+    RUTA NOMBRERUTA, TE PERMITE
+    """
+
+    #* Input hidden, de agregar sorteos para mantener actualizados los sorteso con el mes y fechas correspondientes.
+    #* Es decir, nos ayudará para que el codigo del método ListaSorteos() pueda hacer la parte de agregar los sorteos si no lo están
+    actualizarFechasSorteos = request.form['agregarSorteos']
+
+    #* objeto de la clase Sorteos
+    objSorteos = Sorteos(collectionSorteos, session['email'], session['email_id'])
+
+    #* metodo para agregar sorteos
+    (mensaje, listaConSorteos, condicionarparticipacion) = objSorteos.listaSorteos(actualizarFechasSorteos)
+
+
+    
+    return render_template('listaSorteos.html', mensaje=mensaje, listaSorteos=listaConSorteos, participacion=condicionarparticipacion)
+
+#*******************************************
+
+@app.route('/sorteo')
+def sorteo():
+
+    """
+    RUTA NOMBRERUTA, TE PERMITE
+    """
+
+    return render_template('sorteo.html')
+
+
+# ******************************************
+
+
+@app.route('/sorteo', methods=['POST'])
+def sorteoDatos():
+
+    """
+    RUTA NOMBRERUTA, TE PERMITE
+    """
+
+    #* input hidden
+    participar = request.form['participar']
+
+    #* input con las fechas de la semana:
+
+    semana = request.form['semana']
+
+
+    #?????????????????????????????????????????????????????????
+
+    #* objeto de la clase Sorteos
+    objSorteos = Sorteos(collectionEmail, session['email'], session['email_id'])
+
+    #* metodo para agregar sorteos
+    (mensaje, listaUsuarios) = objSorteos.sorteo(participar, semana)
+
+    return render_template('sorteo.html', mensaje=mensaje, usuarios=listaUsuarios)
+
+#*******************************************
+
+#* Si necesitas más rutas, copia y pega estas dos plantillas de rutas
+
+# @app.route('/nuevaRuta')
+# def nuevaRuta():
+
+    """
+    RUTA NOMBRERUTA, TE PERMITE
+    """
+
+#     return render_template('nuevaRuta.html')
+
+
+# ******************************************
+
+
+# @app.route('/nuevaRuta', methods=['POST'])
+# def nuevaRutaDatos():
+
+    """
+    RUTA NOMBRERUTA, TE PERMITE
+    """
+
+# return render_template('nuevaRuta.html')
+
+# ******************************************
+#*PAGE ERROR
+@app.errorhandler(404)
+def page_no_found(error):
+
+    """
+    RUTA DE 'PAGINA NO ENCONTRADA', POR SI ALGUIEN PONE LA RUTA DE URL MAL.
+    """
+    return '<h1> Pagina no encontrada, siga buscando</h1>'
+
+
+# ******************************************
+#*MAIN
+if __name__ == "__main__":
+
+    """
+    MAIN, PARA INICIAR MAIN.PY
+    """
+    #* FORMA MAIN CON DOCKER
+    # app.run('0.0.0.0', '5000', debug=True)
+    #* Como funciona?R/: Basicamente estamos definiendo el puerto local, 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
---
+++ src/paquete/Sorteos.py
@@ -0,0 +1,245 @@
+
+#* RANDOM
+import random
+#* OS
+import os
+#* DATETIME
+import datetime
+
+
+class Sorteos():
+
+    """
+    CLASE NOMBRECLASE, NOS PERMITIRÁ APLICAR LOS METODOS A LO QUE NECESITEMOS MEDIANTE EL OBJETO
+    """
+
+    def __init__(self, collection, email, email_id):
+
+        """
+        CONTRUCTOR DE LA CLASE NOMBRECLASE
+        """
+
+        self.inicio = 0
+
+        #* Collection mongoDB
+        self.collection = collection
+
+        #* Usuario, solo si la vamos a usar un login en la aplicación
+        self.email = email
+
+        #* email_id, lo usarmeos para las querys de mongoDB
+        self.email_id = email_id
+
+    def listaSorteos(self, actualizarFechaSorteo):
+
+        #* Fecha actual
+        x = datetime.datetime.now()
+        print(x)
+
+        #* Datos de la fecha actual
+        anyoActual = x.strftime("%Y")
+        mesActual = x.strftime("%m")
+        diaActual = x.strftime("%d")
+        horaActual = x.strftime("%X")
+
+        if actualizarFechaSorteo == '1':
+
+            #* Diccionario que reune todos los eventos por mes, para agregarlo a mongoDB
+            infoSorteos = {
+                'sorteo1':{
+                    'plazo': [f'{anyoActual}-{mesActual}-1', f'{anyoActual}-{mesActual}-7'],
+                    'premio': 'coche'
+                },
+                'sorteo2':{
+                    'plazo': [f'{anyoActual}-{mesActual}-8', f'{anyoActual}-{mesActual}-15'],
+                    'premio': 'moto'
+                },
+                'sorteo3':{
+                    'plazo': [f'{anyoActual}-{mesActual}-16', f'{anyoActual}-{mesActual}-23'],
+                    'premio': 'barco'
+                },
+                #* Lo deje hasta el día 28, porque así no afectará el mes febrero a los sorteos por el tema de que los dias de febrero que son menos.
+                'sorteo4':{
+                    'plazo': [f'{anyoActual}-{mesActual}-24', f'{anyoActual}-{mesActual}-28'],
+                    'premio': 'mansión'
+                }
+            }
+
+            # #* diccionario que nos servirá para hacer la busqueda de mongoDB en la variable "buscarSorteos"
+            buscarDicc = {
+                'sorteos.sorteo1.plazo': [f'{anyoActual}-{mesActual}-1', f'{anyoActual}-{mesActual}-7']
+            }
+
+            # #* Esta variable con la query de FINd de mongoDB, nos ayudará a que no se repitan registros de los mismos eventos que ya han sido agregados
+            buscarSorteos = self.collection.find(buscarDicc)
+
+            if list(buscarSorteos) != []:
+
+                #* variable con query de mongoDB para obtener los sorteos
+                verSorteos = self.collection.find(buscarDicc)
+
+                #* lista que contendrá los sorteos
+                listaConSorteos = []
+
+                for i in list(verSorteos):
+
+                    listaConSorteos.append(i)
+
+                #* dias de los plazos de  los sorteos
+                diaInicial1 = int(listaConSorteos[0]['sorteos']['sorteo1']['plazo'][0].split('-')[2])
+                diaFinal1 = int(listaConSorteos[0]['sorteos']['sorteo1']['plazo'][1].split('-')[2])
+
+                diaInicial2 = int(listaConSorteos[0]['sorteos']['sorteo2']['plazo'][0].split('-')[2])
+                diaFinal2 = int(listaConSorteos[0]['sorteos']['sorteo2']['plazo'][1].split('-')[2])
+
+                diaInicial3 = int(listaConSorteos[0]['sorteos']['sorteo3']['plazo'][0].split('-')[2])
+                diaFinal3 = int(listaConSorteos[0]['sorteos']['sorteo3']['plazo'][1].split('-')[2])
+
+                diaInicial4 = int(listaConSorteos[0]['sorteos']['sorteo4']['plazo'][0].split('-')[2])
+                diaFinal4 = int(listaConSorteos[0]['sorteos']['sorteo4']['plazo'][1].split('-')[2])
+
+                sorteo1 = [False, '']
+                sorteo2 = [False, '']
+                sorteo3 = [False, '']
+                sorteo4 = [False, '']
+                condicionarParticipar = False
+
+                if (int(diaActual) < diaFinal1 and int(diaActual) > diaInicial1):
+
+                    sorteo1[0] = True
+
+                    sorteo1[1] = f'Ya existen/fueron agregados sorteos de este mes {mesActual}, puedes participar en este sorteo1'
+
+                    condicionarParticipar = True
+                else:
+
+                    sorteo1[0] = False
+
+                    sorteo1[1] = f'El dia de hoy es: {diaActual} por lo que ya no está disponible el sorteo1'
+
+
+                    condicionarParticipar = False
+
+                #????????????????????????????????????????????????????????????????????????
+
+                if (int(diaActual) < diaFinal2 and int(diaActual) > diaInicial2):
+
+                    sorteo2[0] = True 
+
+                    sorteo2[1] = f'Ya existen/fueron agregados sorteos de este mes {mesActual}, puedes participar en este sorteo2'
+
+                    condicionarParticipar = True
+
+
+                else:
+
+                    sorteo2[0] = False
+
+                    sorteo2[1] = f'El dia de hoy es: {diaActual} por lo que ya no está disponible el sorteo2'
+
+                    condicionarParticipar = False
+
+
+                #????????????????????????????????????????????????????????????????????????
+
+                if (int(diaActual) < diaFinal3 and int(diaActual) > diaInicial3):
+
+                    sorteo3[0] = True
+
+                    sorteo3[1] = f'Ya existen/fueron agregados sorteos de este mes {mesActual}, puedes participar en este sorteo3'
+
+                    condicionarParticipar = True
+
+                else:
+
+                    sorteo3[0] = False
+
+                    sorteo3[1] = f'El dia de hoy es: {diaActual} por lo que ya no está disponible el sorteo3'
+
+                    condicionarParticipar = False
+
+                #????????????????????????????????????????????????????????????????????????
+
+                if (int(diaActual) < diaFinal4 and int(diaActual) > diaInicial4):
+
+                    sorteo4[0] = True
+
+                    sorteo4[1] = f'Ya existen/fueron agregados sorteos de este mes {mesActual}, puedes participar en este sorteo4'
+
+                    condicionarParticipar = True
+
+                else:
+
+                    sorteo4[0] = False
+
+                    sorteo4[1] = f'El dia de hoy: {diaActual} por lo que ya no está disponible el sorteo4'
+
+                    condicionarParticipar = False
+
+                return {'sorteo1': sorteo1, 'sorteo2': sorteo2, 'sorteo3': sorteo3, 'sorteo4': sorteo4}, listaConSorteos, condicionarParticipar
+
+            else:
+
+                #* agregamos los sorteos del mes
+                agregarSorteos = self.collection.insert_one({'sorteos': infoSorteos})
+
+                #* lista para agregar el id_sorteo
+                lista_Sorteo_id = []
+
+                buscarSorteos2 = self.collection.find(buscarDicc)
+
+                for i in list(buscarSorteos2):
+
+                    lista_Sorteo_id.append(str(i['_id']))
+
+                
+                agregarSorteo_id = self.collection.update_one(buscarDicc, {'$set': {'sorteo_id': lista_Sorteo_id[0]}})
+
+                #* Este return solo funcionará cuando estemos en un nuevo mes, y el metodo agregue los sorteos actualizados del mes actual
+                #* en el que estemos, ya que si ya fue agregado, hará el return anterior, es decir este: return f'Ya existen sorteos de este mes {mesActual}'
+                return {'msg': f'Se han agregado los sorteos del mes: {mesActual}'}, [], None
+
+    def sorteo(self, participar, semana):
+
+        agregarParticipante = self.collection.update_one({'email_id': self.email_id}, {'$set': {'sorteo_id': participar}})
+
+        agregarSemana = self.collection.update_one({'email_id': self.email_id}, {'$set': {'sorteoSemana': semana}})
+
+        listaInfoUsuario = []
+
+        infoUsuario = self.collection.find({'email_id': self.email_id})
+
+        for i in list(infoUsuario):
+
+            listaInfoUsuario.append(i)
+
+        # if listaInfoUsuario != []:
+
+            #* SORTEO DE LA SEMANA
+
+            #******************************+
+
+        listaTodosUsuarios = []
+
+        all_users = self.collection.find({})
+
+        for i in list(all_users):
+
+            listaTodosUsuarios.append(i)
+
+        print(listaTodosUsuarios)
+
+        return 'GRACIAS POR PARTICIPAR, ESPERA EL RESULTADO A FINAL DE LA SEMANA', listaTodosUsuarios
+
+            # for sort in listaTodosUsuarios:
+
+            #     if 
+
+
+
+
+            #*******************************
+
+
+        # return 'GRACIAS POR PARTICIPAR, ESPERA EL RESULTADO A FINAL DE LA SEMANA', listaTodosUsuarios
+
