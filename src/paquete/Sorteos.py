2a0834468639feaaea9b83515328e38d52d34c3b
matiusgg <newpowerexperience2016@gmail.com>
2020-03-30 19:04:46 +0200

    Se agregaron nuevas cosas como condicionales y bucles de jinja que me ayudarán con el contenido que debeb aparecer

--- src/paquete/Sorteos.py
+++ src/paquete/Sorteos.py
@@ -29,9 +29,7 @@ class Sorteos():
 
         #* email_id, lo usarmeos para las querys de mongoDB
         self.email_id = email_id
-
-
-
+        
     def listaSorteos(self, actualizarFechaSorteo):
 
         #* Fecha actual
